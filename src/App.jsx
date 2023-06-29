672912462d9b4d3d0787fa69effba9410970cdbd
SivaSurender <e.surender14@gmail.com>
2023-06-29 14:03:45 +0530

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,5 @@
-import Footer from "../UI/Footer";
-import Header from "../UI/Header";
-import Menu from "../UI/Menu";
-
 function App() {
-  return (
-    <div className="container">
-      <Header />
-      <Menu />
-      <Footer />
-    </div>
-  );
+  return <p>Test</p>;
 }
 
 export default App;
