86f3292f21c562d8108c638d26f3619bbe7fd694
SivaSurender <e.surender14@gmail.com>
2023-07-01 16:30:01 +0530

    added code

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 10, packed: true },
+];
+
 function App() {
   return (
     <div className="app">
@@ -22,14 +28,28 @@ function Form() {
 function PackingList() {
   return (
     <div className="list">
-      <h3>List</h3>
+      <ul>
+        {initialItems.map((each, index) => (
+          <Item item={each} />
+        ))}
+      </ul>
     </div>
+  );
+}
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
   );
 }
 
 function Stats() {
   return (
-    <footer>
+    <footer className="stats">
       <em>You have X items on your list and you already packed X (X%)</em>
     </footer>
   );
