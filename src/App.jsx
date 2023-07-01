7ac6cfdcc6f5a47b9379df27ac9f2ad152a9af2a
SivaSurender <e.surender14@gmail.com>
2023-07-01 16:17:23 +0530

    added code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
-import Footer from "../UI/Footer";
-import Header from "../UI/Header";
-import Menu from "../UI/Menu";
-
 function App() {
   return (
-    <div className="container">
-      <Header />
-      <Menu />
-      <Footer />
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
 
+function Logo() {
+  return <h1>Far Away C-List</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return (
+    <div className="list">
+      <h3>List</h3>
+    </div>
+  );
+}
+
+function Stats() {
+  return (
+    <footer>
+      <em>You have X items on your list and you already packed X (X%)</em>
+    </footer>
+  );
+}
 export default App;
