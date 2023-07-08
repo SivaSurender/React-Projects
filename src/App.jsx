3b971a177255bc75db5ea5b8869c7b8ce89a9f07
SivaSurender <e.surender14@gmail.com>
2023-07-08 17:59:35 +0530

    Initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-import Footer from "../UI/Footer";
-import Header from "../UI/Header";
-import Menu from "../UI/Menu";
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+import React from "react";
 
 function App() {
-  return (
-    <div className="container">
-      <Header />
-      <Menu />
-      <Footer />
-    </div>
-  );
+  return <div>App</div>;
 }
 
 export default App;
