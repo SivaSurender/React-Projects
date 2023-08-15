2c9ed7025944113b53d4cda3a099810e7e8f9c02
SivaSurender <e.surender14@gmail.com>
2023-08-15 19:14:31 +0530

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,16 @@
 import { useState } from "react";
+import Header from "./Header";
+import Main from "./components/Main";
 
-const messages = [
-  "Learn React ⚛️",
-  "Apply for jobs 💼",
-  "Invest your new income 🤑",
-];
 function App() {
-  // const stepCounter = 3;
-  const [stepCounter, setStepCounter] = useState(1);
-
   return (
-    <div classNameName="steps">
-      <div className="numbers">
-        <div className={stepCounter >= 1 ? "active" : ""}>1</div>
-        <div className={stepCounter >= 2 ? "active" : ""}>2</div>
-        <div className={stepCounter >= 3 ? "active" : ""}>3</div>
-      </div>
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
 
-      <p className="message">
-        Step {stepCounter} : {messages[stepCounter - 1]}
-      </p>
-
-      <div className="buttons">
-        <button
-          onClick={() => stepCounter > 1 && setStepCounter((prev) => prev - 1)}
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-        >
-          Previous
-        </button>
-
-        <button
-          onClick={() => stepCounter < 3 && setStepCounter((prev) => prev + 1)}
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-        >
-          Next
-        </button>
-      </div>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
