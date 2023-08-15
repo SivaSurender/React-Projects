85cb3be35c3c3dd7ce0dad6babf51b5414a29e2e
SivaSurender <e.surender14@gmail.com>
2023-08-15 19:32:04 +0530

    reducer setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,40 @@
-import { useState } from "react";
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./components/Main";
 
+const initialState = {
+  questions: [],
+  status: "loading",
+};
 function App() {
+  const [state, dispatch] = useReducer(reducerFn, initialState);
+
+  function reducerFn(state, action) {
+    switch (action.type) {
+      case "dataFetched": {
+        return { ...state, questions: action.payload, status: "ready" };
+      }
+      default: {
+        return initialState;
+      }
+    }
+  }
+
+  useEffect(() => {
+    const getQuest = async function () {
+      try {
+        const res = await fetch("http://localhost:3000/questions");
+        const data = await res.json();
+        console.log(data, "1");
+        dispatch({ type: "dataFetched", payload: data });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getQuest();
+  }, []);
+
   return (
     <div className="app">
       <Header />
